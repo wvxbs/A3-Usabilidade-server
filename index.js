eebafb97fac55c851cc254b734531b7bb5fd27ea
wvxbs <gabriel.ferreira7854@gmail.com>
2022-11-25 11:39:00 -0300

    addded cors to server config

--- index.js
+++ index.js
@@ -19,14 +19,16 @@ mongoose
 
 dotenv.config()
 
+app.use(cors())
+
 app.use(bodyParser.json())
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*")
-  res.header("Access-Control-Allow-Methods", "*")
-  res.header("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept")
-  next()
-})
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*")
+//   res.header("Access-Control-Allow-Methods", "*")
+//   res.header("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept")
+//   next()
+// })
 
 app.use('/person', Persons)
 
