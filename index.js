258d017badfce1ee57621d876d7bbbf0d9604a23
wvxbs <gabriel.ferreira7854@gmail.com>
2022-11-27 19:33:49 -0300

    Cors enabled

--- index.js
+++ index.js
@@ -21,12 +21,12 @@ dotenv.config()
 
 app.use(bodyParser.json())
 
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*")
-//   res.header("Access-Control-Allow-Methods", "*")
-//   res.header("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept")
-//   next()
-// })
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Methods", "*")
+  res.header("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept")
+  next()
+})
 
 app.use('/person', Persons)
 
