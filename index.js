80bd6a1c930bad2e2b5dbe89f3c7ef3a4970c4ba
wvxbs <gabriel.ferreira7854@gmail.com>
2022-11-29 02:31:03 -0300

    index updated

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(bodyParser.json())
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*")
-  res.header("Access-Control-Allow-Methods", "GET, POST, DELETE")
+  res.header("Access-Control-Allow-Methods", "*")
   res.header("Access-Control-Allow-Headers", "*")
   res.header("Access-Control-Allow-Credentials", "true")
 
