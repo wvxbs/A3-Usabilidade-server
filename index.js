d50ec92844cf0b52121dd7253dacbc904c498d15
wvxbs <gabriel.ferreira7854@gmail.com>
2022-11-29 00:39:20 -0300

    index updated

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true
   })
   .then(result => {
-    console.log('MongoDB Connected')
+    console.log('mongodb successfully connected!')
   })
   .catch(error => {
     console.log(error)
