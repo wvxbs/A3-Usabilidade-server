6fcf069e65f366a77e32e1ac3146499632e1ed60
wvxbs <gabriel.ferreira7854@gmail.com>
2022-11-29 01:56:41 -0300

    Persons updated

--- routes/Persons.js
+++ routes/Persons.js
@@ -29,7 +29,7 @@ router.get('/', (req, res) => {
     .catch(error => res.status(500).json(error))
 })
 
-router.delete('/', (req, res) => {
+router.delete('/delete', (req, res) => {
   Person.findOneAndDelete({ _id: req.body.data._id})
     .then(Person => {
       res.json(Person);
