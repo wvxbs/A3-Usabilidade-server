72de1fd41b8d97ce0866d8d863a7020b87917295
wvxbs <gabriel.ferreira7854@gmail.com>
2022-11-29 04:09:30 -0300

    .

--- index.js
+++ index.js
@@ -21,9 +21,9 @@ dotenv.config()
 app.use(bodyParser.json())
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*")
-  res.header("Access-Control-Allow-Methods", "*")
-  res.header("Access-Control-Allow-Headers", "*")
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
   res.header("Access-Control-Allow-Credentials", "true")
 
   next()
